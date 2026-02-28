cee066da9acbfecf41fa3deb5524be2cfa445efc
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2026-02-28 16:05:03 +0600

    update dir

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/sajibmalek/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sajibmalek/Documents/GitHub/AquaGuard"
+export "FLUTTER_APPLICATION_PATH=/Users/sajibmalek/Documents/GitHub/AquaGuard/aqua_guard"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
